6f8f8497fc2118ba8211886933927a480be69fb2
ZShadow01 <zshadow.sniper01@gmail.com>
2024-09-08 20:22:31 +0200

    Changed colour template + decreased height on homepage

--- app/web/public/js/main.js
+++ app/web/public/js/main.js
@@ -1,22 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const characterImages = document.getElementsByClassName('character-image');
-    for (let i = 0; i < characterImages.length; i++) {
-        characterImages.item(i).style.opacity = 0;
-    }
+let playerIndex = 0;
 
-    const duration = 400;
-    let index = 0;
-    let timeout;
-    const startChainAnimation = () => {
-        if (index < characterImages.length) {
-            characterImages.item(index).style.opacity = null;
-            characterImages.item(index).style.animation = `fade-in ${duration}ms ease`;
-            index++;
-            timeout = setTimeout(startChainAnimation, 500);
-        } else {
-            clearTimeout(timeout);
-        }
-    };
+
+document.addEventListener('DOMContentLoaded', function() {
+    const characterImages = document.getElementsByClassName('player-character-image');
+    const playerAnimationDelay = 400;
+    for (let i = 0; i < characterImages.length; i++) {
+        characterImages.item(i).style.animationDelay = playerAnimationDelay * i + 'ms';
+    }
     
-    timeout = setTimeout(startChainAnimation, 50);
+    setTimeout(() => {
+        nextPlayer();
+        setInterval(nextPlayer, 10_000);  // 10_000
+    }, 2500);
 });
+
+
+function nextPlayer() {
+    const characterContainers = document.getElementsByClassName('player-character-container');
+    const playerInfoContainers = document.getElementsByClassName('player-info-container');
+
+    document.querySelector('.player-character-container.active')?.classList.remove('active');
+    document.querySelector('.player-info-container.active')?.classList.remove('active');
+
+    characterContainers.item(playerIndex).classList.add('active');
+    playerInfoContainers.item(playerIndex).classList.add('active');
+
+    playerIndex = (playerIndex + 1) % characterContainers.length;
+}
